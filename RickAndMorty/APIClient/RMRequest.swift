af0051d44e54c57e49ac9468debd808f190d5d16
Nicholas Gilbert <nicholasgilbert21@yahoo.com>
2023-04-24 18:38:04 -0400

    Added docstrings and API related files

---
+++ RickAndMorty/APIClient/RMRequest.swift
@@ -0,0 +1,13 @@
+//
+//  RMRequest.swift
+//  RickAndMorty
+//
+//  Created by Nicholas Gilbert on 4/21/23.
+//
+
+import Foundation
+
+/// Object that represents a single API call
+final class RMRequest {
+    
+}
